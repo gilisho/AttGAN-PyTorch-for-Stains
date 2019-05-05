9d60237df76374e419ebdc73ac1c3f47ce38f074
glukhman <michaelg@cloudify.co>
2019-05-05 15:57:09 +0300

    Added normal dist. of spots, correct transparency and more constants

--- data_set/consts.py
+++ data_set/consts.py
@@ -7,4 +7,20 @@ img_size = (500, 500)  # size of image
 img_color = 'white'  # background color of image
 
 txt_path = 'assets/alice_in_wonderland.txt'
-spot_path = 'assets/images/black-circle.png'
+spot_path = 'assets/spots'
+font_path = 'assets/fonts'
+eligible_image_formats = ('.png', '.webp', '.gif', '.tif', '.tiff')
+
+min_text_lines = 5
+max_text_lines = 20
+
+min_fontsize = 15
+max_fontsize = 30
+
+min_spotheight = 5      # in % of the image height
+max_spotheight = 50
+
+min_spot_opacity = 30   # in %
+max_spot_opacity = 70
+
+mean_spots_per_image = 3    # for normal distribution
--- data_set/read_txt_file_utils.py
+++ data_set/read_txt_file_utils.py
@@ -1,19 +1,21 @@
 import random
 
 
-def read_txt_file(filename):
+def read_txt_file(filename, minlines, maxlines):
     file = open(filename, "r")
     output_text = ''
 
     while len(output_text) == 0:  # make sure that output_text will not be empty
         lines = file.readlines()
-        num_of_lines_in_files = len(lines)
+        num_of_lines_in_file = len(lines)
 
-        num_of_lines_in_output_text = random.randint(1, 4)  # number of lines of text is between 1 to 3
+        num_of_lines_in_output_text = random.randint(minlines, maxlines)
 
         # take randomly a line and add it to output_text
-        for i in range(num_of_lines_in_output_text):
-            line_num = random.randint(0, num_of_lines_in_files + 1)  # line num to read from text
+        for _ in range(num_of_lines_in_output_text):
+            line_num = random.randint(0, num_of_lines_in_file + 1)  # line num to read from text
+            if line_num >= num_of_lines_in_file:       # file has ended 
+                break
             output_text += lines[line_num]
 
     file.close()
