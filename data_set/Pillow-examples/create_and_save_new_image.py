00030f804f1ba08e1b4267e41e6fa2d2713b3912
gilisho <gilisho@users.noreply.github.com>
2019-07-01 00:33:38 +0300

    delete unnecessary files

--- data_set/Pillow-examples/create_and_save_new_image.py
+++
@@ -1,8 +0,0 @@
-from PIL import Image
-
-mode = 'RGB'
-size = (300, 300)  # size of image
-color = (73, 109, 137)  # background color
-
-img = Image.new(mode, size, color)
-img.save('./output_images/create_and_save_new_image.png')
